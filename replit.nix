5baaa65aefbb4c350260baee4b91620643267762
Esketch <esketch@gmail.com>
2025-07-23 12:27:08 +0900

    feat: Replit 환경 구성 및 데이터베이스 설정 수정

--- replit.nix
+++ replit.nix
@@ -1,7 +1,9 @@
 { pkgs }: {
   deps = [
-    pkgs.rubyPackages.bundler,
+    pkgs.ruby_3_2
+    pkgs.rubyPackages_3_2.bundler,
     pkgs.postgresql,
+    pkgs.pkg-config,
     pkgs.libyaml,
     pkgs.libffi
   ];
