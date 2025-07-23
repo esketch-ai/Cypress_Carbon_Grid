9ad5df00755c03a3a59bc57487805c83917be57a
Esketch <esketch@gmail.com>
2025-07-23 13:37:13 +0900

    fix: Add dev libraries for pg and psych gems

--- replit.nix
+++ replit.nix
@@ -2,9 +2,9 @@
   deps = [
     pkgs.ruby_3_2,
     pkgs.rubyPackages_3_2.bundler,
-    pkgs.postgresql,
+    pkgs.postgresql.lib,
+    pkgs.libyaml.dev,
     pkgs.pkg-config,
-    pkgs.libyaml,
     pkgs.libffi,
     pkgs.gcc
   ];
