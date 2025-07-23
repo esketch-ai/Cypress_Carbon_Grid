3c369776dbf36fc044fc4edea0bbb2d12a5b00dc
Esketch <esketch@gmail.com>
2025-07-23 17:46:46 +0900

    fix: Fix syntax error in replit.nix

--- replit.nix
+++ replit.nix
@@ -1,14 +1,14 @@
 { pkgs }: {
   deps = [
-    pkgs.ruby_3_2,
-    pkgs.rubyPackages_3_2.bundler,
-    pkgs.postgresql.lib,
-    pkgs.libyaml.dev,
-    pkgs.pkg-config,
-    pkgs.libffi,
-    pkgs.gcc,
-    pkgs.docker-compose,
-    pkgs.nodejs,
+    pkgs.ruby_3_2
+    pkgs.rubyPackages_3_2.bundler
+    pkgs.postgresql.lib
+    pkgs.libyaml.dev
+    pkgs.pkg-config
+    pkgs.libffi
+    pkgs.gcc
+    pkgs.docker-compose
+    pkgs.nodejs
     pkgs.sqlite
   ];
   services.docker = {
